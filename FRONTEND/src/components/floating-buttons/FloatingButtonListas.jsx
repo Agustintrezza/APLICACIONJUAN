b1c750110a57949936cee6589c1774323c3ad09b
Agustintrezza <agustin.trezza@gmail.com>
2025-01-03 22:10:39 -0300

    Mas responsive

--- FRONTEND/src/components/floating-buttons/FloatingButtonListas.jsx
+++ FRONTEND/src/components/floating-buttons/FloatingButtonListas.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { FaEdit } from 'react-icons/fa'
+import { FaPlus } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import FormularioListas from '../listas/FormularioListas'
 
@@ -25,7 +25,7 @@ const FloatingButtonEdit = ({ listaToEdit, onUpdate }) => {
         className="fixed bg-[#293e68] mt-3 top-2 right-16 text-white p-2 rounded-md shadow-lg z-50"
         title="Editar Lista"
       >
-        <FaEdit size={18} />
+        <FaPlus size={18} />
       </button>
 
       {/* Overlay */}
@@ -43,7 +43,7 @@ const FloatingButtonEdit = ({ listaToEdit, onUpdate }) => {
         }`}
       >
         <div className="flex justify-between items-center mb-4">
-          <h3 className="text-lg font-semibold text-gray-800">Editar Lista</h3>
+          <h3 className="text-lg font-semibold text-gray-800">Crear Lista</h3>
           <button
             onClick={toggleSidebar}
             className="text-gray-500 hover:text-gray-700"
