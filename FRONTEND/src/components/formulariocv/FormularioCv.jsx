e64135fdc6628abe3c5a2c36867595b40edfc9d5
Agustintrezza <agustin.trezza@gmail.com>
2025-01-11 00:57:17 -0300

    Ajustes visuales en los checkbox, se achicaron

--- FRONTEND/src/components/formulariocv/FormularioCv.jsx
+++ FRONTEND/src/components/formulariocv/FormularioCv.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import PaisSelect from "../paisselect/PaisSelect";
-import RubrosSelect from "../rubrosselect/RubrosSelect";
+import RubrosSelect from "../rubrosselect/RubrosSelect"
 import ListasSelect from "../listasselect/ListasSelect";
-import { useState, useEffect } from "react";
+import {useState, useEffect} from 'react';
 
 const FormularioCv = ({
   formData,
@@ -14,7 +14,7 @@ const FormularioCv = ({
   handleFileChange,
   handleSubmit,
   fileInputRef, // Indicador para activar el Skeleton
-  isEditMode = false,
+  isEditMode= false
 }) => {
   const [isLoading, setIsLoading] = useState(true);
 
@@ -27,6 +27,7 @@ const FormularioCv = ({
     return () => clearTimeout(timeout); // Limpia el timeout al desmontar el componente
   }, []);
 
+
   const [selectedLista, setSelectedLista] = useState("");
 
   useEffect(() => {
@@ -37,81 +38,64 @@ const FormularioCv = ({
     setFormData((prevData) => {
       const updatedIdiomas = prevData.idiomas.includes(language)
         ? prevData.idiomas.filter((idioma) => idioma !== language) // Elimina si ya está seleccionado
-        : [...prevData.idiomas, language]; // Agrega si no está seleccionado
-      return { ...prevData, idiomas: updatedIdiomas };
-    });
-  };
+        : [...prevData.idiomas, language] // Agrega si no está seleccionado
+      return { ...prevData, idiomas: updatedIdiomas }
+    })
+  }
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="bg-white p-6 rounded-lg shadow-md w-full"
-    >
+    <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg shadow-md w-full">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         {/* Columna 1 */}
         <div className="space-y-4">
-          <div className="grid grid-cols-2 gap-4">
-            <div>
-              <label
-                htmlFor="nombre"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Nombre
-              </label>
-              {isLoading ? (
-                <Skeleton height={40} />
-              ) : (
-                <input
-                  type="text"
-                  id="nombre"
-                  name="nombre"
-                  value={formData.nombre}
-                  onChange={handleChange}
-                  className={`w-full p-2 border text-sm rounded-lg ${
-                    errors.nombre ? "border-red-500" : "border-gray-300"
-                  }`}
-                />
-              )}
-              {errors.nombre && (
-                <p className="text-red-500 text-sm mt-1">{errors.nombre}</p>
-              )}
-            </div>
-
-            <div>
-              <label
-                htmlFor="apellido"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Apellido
-              </label>
-              {isLoading ? (
-                <Skeleton height={40} />
-              ) : (
-                <input
-                  type="text"
-                  id="apellido"
-                  name="apellido"
-                  value={formData.apellido}
-                  onChange={handleChange}
-                  className={`w-full p-2 border text-sm rounded-lg ${
-                    errors.apellido ? "border-red-500" : "border-gray-300"
-                  }`}
-                />
-              )}
-              {errors.apellido && (
-                <p className="text-red-500 text-sm mt-1">{errors.apellido}</p>
-              )}
-            </div>
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <label htmlFor="nombre" className="block mb-2 text-sm font-medium text-gray-900">
+              Nombre
+            </label>
+            {isLoading ? (
+              <Skeleton height={40} />
+            ) : (
+              <input
+                type="text"
+                id="nombre"
+                name="nombre"
+                value={formData.nombre}
+                onChange={handleChange}
+                className={`w-full p-2 border text-sm rounded-lg ${
+                  errors.nombre ? "border-red-500" : "border-gray-300"
+                }`}
+              />
+            )}
+            {errors.nombre && <p className="text-red-500 text-sm mt-1">{errors.nombre}</p>}
           </div>
 
           <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900">
-              Género
+            <label htmlFor="apellido" className="block mb-2 text-sm font-medium text-gray-900">
+              Apellido
             </label>
+            {isLoading ? (
+              <Skeleton height={40} />
+            ) : (
+              <input
+                type="text"
+                id="apellido"
+                name="apellido"
+                value={formData.apellido}
+                onChange={handleChange}
+                className={`w-full p-2 border text-sm rounded-lg ${
+                  errors.apellido ? "border-red-500" : "border-gray-300"
+                }`}
+              />
+            )}
+            {errors.apellido && <p className="text-red-500 text-sm mt-1">{errors.apellido}</p>}
+          </div>
+          </div>
+
+          <div>
+            <label className="block mb-2 text-sm font-medium text-gray-900">Género</label>
             <div className="flex space-x-4">
               {isLoading
-                ? [1, 2].map((_, index) => (
-                    <Skeleton key={index} height={20} width={100} />
-                  ))
+                ? [1, 2].map((_, index) => <Skeleton key={index} height={20} width={100} />)
                 : ["Masculino", "Femenino"].map((option) => (
                     <label key={option} className="flex items-center">
                       <input
@@ -126,73 +110,60 @@ const FormularioCv = ({
                     </label>
                   ))}
             </div>
-            {errors.genero && (
-              <p className="text-red-500 text-sm mt-1">{errors.genero}</p>
+            {errors.genero && <p className="text-red-500 text-sm mt-1">{errors.genero}</p>}
+          </div>
+          
+
+          <div className="grid grid-cols-2 gap-4">
+          <div>
+            <label htmlFor="edad" className="block mb-2 text-sm font-medium text-gray-900">
+              Edad
+            </label>
+            {isLoading ? (
+              <Skeleton height={40} />
+            ) : (
+              <input
+                type="number"
+                id="edad"
+                name="edad"
+                value={formData.edad || ""}
+                onChange={handleChange}
+                className={`w-full p-2 border text-sm rounded-lg ${
+                  errors.edad ? "border-red-500" : "border-gray-300"
+                }`}
+                min="18"
+                max="100"
+              />
             )}
+            {errors.edad && <p className="text-red-500 text-sm mt-1">{errors.edad}</p>}
+          </div>
+
+          <div>
+            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">
+              Email
+            </label>
+            {isLoading ? (
+              <Skeleton height={40} />
+            ) : (
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email || ""}
+                onChange={handleChange}
+                className={`w-full p-2 border text-sm rounded-lg ${
+                  errors.email ? "border-red-500" : "border-gray-300"
+                }`}
+              />
+            )}
+            {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
+          </div>
+
           </div>
 
           <div className="grid grid-cols-2 gap-4">
             <div>
-              <label
-                htmlFor="edad"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Edad
-              </label>
-              {isLoading ? (
-                <Skeleton height={40} />
-              ) : (
-                <input
-                  type="number"
-                  id="edad"
-                  name="edad"
-                  value={formData.edad || ""}
-                  onChange={handleChange}
-                  className={`w-full p-2 border text-sm rounded-lg ${
-                    errors.edad ? "border-red-500" : "border-gray-300"
-                  }`}
-                  min="18"
-                  max="100"
-                />
-              )}
-              {errors.edad && (
-                <p className="text-red-500 text-sm mt-1">{errors.edad}</p>
-              )}
-            </div>
-
-            <div>
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Email
-              </label>
-              {isLoading ? (
-                <Skeleton height={40} />
-              ) : (
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={formData.email || ""}
-                  onChange={handleChange}
-                  className={`w-full p-2 border text-sm rounded-lg ${
-                    errors.email ? "border-red-500" : "border-gray-300"
-                  }`}
-                />
-              )}
-              {errors.email && (
-                <p className="text-red-500 text-sm mt-1">{errors.email}</p>
-              )}
-            </div>
-          </div>
-
-          <div className="grid grid-cols-2 gap-4">
-            <div>
-              <label
-                htmlFor="telefonoFijo"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
+              <label htmlFor="telefonoFijo" className="block mb-2 text-sm font-medium text-gray-900">
                 Teléfono Fijo
               </label>
               {isLoading ? (
@@ -209,10 +180,7 @@ const FormularioCv = ({
               )}
             </div>
             <div>
-              <label
-                htmlFor="celular"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
+              <label htmlFor="celular" className="block mb-2 text-sm font-medium text-gray-900">
                 Teléfono Celular
               </label>
               {isLoading ? (
@@ -229,9 +197,7 @@ const FormularioCv = ({
                   }`}
                 />
               )}
-              {errors.celular && (
-                <p className="text-red-500 text-sm mt-1">{errors.celular}</p>
-              )}
+              {errors.celular && <p className="text-red-500 text-sm mt-1">{errors.celular}</p>}
             </div>
           </div>
 
@@ -242,26 +208,16 @@ const FormularioCv = ({
             isLoading={isLoading}
             handleChange={handleChange}
           />
-          <PaisSelect
-            formData={formData}
-            setFormData={setFormData}
-            isLoading={isLoading}
-          />
+        <PaisSelect formData={formData} setFormData={setFormData} isLoading={isLoading} />
         </div>
 
         {/* Columna 2 */}
         <div className="space-y-4">
-          {!isEditMode && (
-            <ListasSelect
-              selectedLista={selectedLista}
-              setSelectedLista={setSelectedLista}
-            />
-          )}
+        {!isEditMode && (
+  <ListasSelect selectedLista={selectedLista} setSelectedLista={setSelectedLista} />
+)}
           <div>
-            <label
-              htmlFor="calificacion"
-              className="block mb-2 text-sm font-medium text-gray-900"
-            >
+            <label htmlFor="calificacion" className="block mb-2 text-sm font-medium text-gray-900">
               Calificación
             </label>
             {isLoading ? (
@@ -288,10 +244,7 @@ const FormularioCv = ({
           </div>
 
           <div>
-            <label
-              htmlFor="nivelEstudios"
-              className="block mb-2 text-sm font-medium text-gray-900"
-            >
+            <label htmlFor="nivelEstudios" className="block mb-2 text-sm font-medium text-gray-900">
               Nivel de Estudios
             </label>
             {isLoading ? (
@@ -314,10 +267,7 @@ const FormularioCv = ({
           </div>
 
           <div>
-            <label
-              htmlFor="experiencia"
-              className="block mb-2 text-sm font-medium text-gray-900"
-            >
+            <label htmlFor="experiencia" className="block mb-2 text-sm font-medium text-gray-900">
               Años de Experiencia
             </label>
             {isLoading ? (
@@ -341,36 +291,29 @@ const FormularioCv = ({
           </div>
 
           <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900">
-              Idiomas
-            </label>
+            <label className="block mb-2 text-sm font-medium text-gray-900">Idiomas</label>
             {isLoading ? (
               <Skeleton count={5} height={20} />
             ) : (
               <div className="grid grid-cols-2 lg:grid-cols-5 gap-2">
-                {["Inglés", "Español", "Portugués", "Italiano", "Francés"].map(
-                  (language) => (
-                    <label key={language} className="flex items-center">
-                      <input
-                        type="checkbox"
-                        value={language}
-                        checked={formData.idiomas.includes(language)} // Chequea si está seleccionado
-                        onChange={() => handleCheckboxChange(language)}
-                        className="mr-2"
-                      />
-                      {language}
-                    </label>
-                  )
-                )}
+                {["Inglés", "Español", "Portugués", "Italiano", "Francés"].map((language) => (
+                  <label key={language} className="flex items-center">
+                    <input
+                      type="checkbox"
+                      value={language}
+                      checked={formData.idiomas.includes(language)}
+                      onChange={() => handleCheckboxChange(language)}
+                      className="mr-2"
+                    />
+                    {language}
+                  </label>
+                ))}
               </div>
             )}
           </div>
 
           <div>
-            <label
-              htmlFor="imagen"
-              className="block mb-2 text-sm font-medium text-gray-900"
-            >
+            <label htmlFor="imagen" className="block mb-2 text-sm font-medium text-gray-900">
               Adjuntar Imagen
             </label>
             {isLoading ? (
@@ -385,16 +328,11 @@ const FormularioCv = ({
                 className="w-full p-2 border text-sm rounded-lg border-blue-300 bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             )}
-            {errors.imagen && (
-              <p className="text-red-500 text-sm mt-1">{errors.imagen}</p>
-            )}
+            {errors.imagen && <p className="text-red-500 text-sm mt-1">{errors.imagen}</p>}
           </div>
 
           <div>
-            <label
-              htmlFor="comentarios"
-              className="block mb-2 text-sm font-medium text-gray-900"
-            >
+            <label htmlFor="comentarios" className="block mb-2 text-sm font-medium text-gray-900">
               Comentarios
             </label>
             {isLoading ? (
