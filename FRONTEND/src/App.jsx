7da549a92702c78412f3f1c4187f8342ce8e9fce
Agustin <agustin.trezza@gmail.com>
2025-02-02 02:15:09 -0300

    Prueba de integracion

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -27,7 +27,7 @@ function AppContent() {
         }
       />
       
-      {/* Rutas protegidas dentro del Layout */}
+      {/* Rutas protegidas dentro del Layout - PRUEBA INTEGRACION */}
       <Route element={<ProtectedRoute><Layout /></ProtectedRoute>}>
         <Route path="/" element={<Home />} />
         <Route path="/crear-cv" element={<CrearCvScreen/>} />
